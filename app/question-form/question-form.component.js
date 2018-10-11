7e003310d582f2843629b5bcc426f4e78cd98963
JT <894507488@qq.com>
2018-10-11 17:00:17 +0800

    question-list-ing

--- app/question-form/question-form.component.js
+++ app/question-form/question-form.component.js
@@ -2,11 +2,22 @@ angular.
     module('questionForm').
         component('questionForm',{
             templateUrl: 'question-form/question-form.template.html',
-            controller: function QuestionFormController(){
-                this.title = '';
-                this.course = '';
-                this.description = '';
-                this.img = '';
+            controller: ['$http',function QuestionFormController($http){
+                var self = this;
+                this.question = {
+                    title : '',
+                    course : '',
+                    description : '',
+                    img : '',
+                    date: 0 
+                }
                 
-            }
+                // $http({
+                //     method:"post",
+                //     url: "questions.php",
+                //     date: self.question
+                // }).success(function(res){
+                //     console.log(res)
+                // })
+            }]
         })
